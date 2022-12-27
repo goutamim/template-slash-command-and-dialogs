cbb2ae917470fff5bb6a422dcd62df4015203d50
goutamim <43399110+goutamim@users.noreply.github.com>
2022-12-27 17:57:46 +0530

    git api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const qs = require('querystring');
 const apiUrl = 'https://slack.com/api';
+const githubUrl = 'https://api.github.com'
 
 const callAPIMethod = async (method, payload) => {
     let data = Object.assign({ token: process.env.SLACK_ACCESS_TOKEN }, payload);
@@ -8,6 +9,16 @@ const callAPIMethod = async (method, payload) => {
     return result.data;
 }
 
+const callgitAPIMethodPost = async () => {
+    console.log('calling github api')
+    let result = await axios.post(`${githubUrl}/repos/softwareartistry/k8s-cli/actions/workflows/penknife-ui-deploy.yml/dispatches`, {"ref":"sprint"}, {
+      headers: { Authorization: "Bearer " + process.env.GITHUB_ACCESS_TOKEN }
+    });
+    console.log('github call api sucessfull');
+  }
+  
+
 module.exports = {
-    callAPIMethod
+    callAPIMethod,
+    callgitAPIMethodPost
 }
