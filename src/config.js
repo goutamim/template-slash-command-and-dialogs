fe04cacebbba4ad9344e89430af7e2dbefcb756c
goutamim <goutu896@gmail.com>
2023-01-10 16:00:57 +0530

    practifly update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const repoTenantList = [
 	{ repo: "penknife-ui", tenant: ["demo", "practifly", "314ecorp"] },
 	{ repo: "penknife-app", tenant: ["demo", "practifly", "314ecorp"]}, //penknife-app and cli same image. no buildname required.
-	{ repo: "practifly-server", tenant: ["demo1"] },
-	{ repo: "practifly-ui", tenant: ["demo1"] },
+	{ repo: "practifly-server", tenant: ["demo1","demo2","mhill","phs","nephc"] }, //DNS practifly.com
+	{ repo: "practifly-ui", tenant: ["demo1","demo2","mhill","phs","nephc"] },
 ];
 
 const apiUrl = "https://slack.com/api";
