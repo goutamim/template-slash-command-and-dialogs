0076c35dc524cb3126b7f664b25dbcc1336f3ba3
goutamim <goutu896@gmail.com>
2023-01-10 17:10:02 +0530

    update penknife-tenant

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const repoTenantList = [
-	{ repo: "penknife-ui", tenant: ["demo", "practifly", "314ecorp"] },
-	{ repo: "penknife-app", tenant: ["demo", "practifly", "314ecorp"]}, //penknife-app and cli same image. no buildname required.
-	{ repo: "practifly-server", tenant: ["demo1","demo2","mhill","phs","nephc"] }, //DNS practifly.com
+	{ repo: "penknife-ui", tenant: ["demo", "practifly", "314ecorp","314einternal"] },
+	{ repo: "penknife-app", tenant: ["demo", "practifly", "314ecorp","314einternal"]}, //penknife-app and cli same image. 
+	{ repo: "practifly-server", tenant: ["demo1","demo2","mhill","phs","nephc"] }, //DNS practifly.com //practifly practifly-cli same image.
 	{ repo: "practifly-ui", tenant: ["demo1","demo2","mhill","phs","nephc"] },
 ];
 
