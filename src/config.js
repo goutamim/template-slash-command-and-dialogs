33511a2829ecb66d69919e62e5f15822a48bfa7d
goutamim <goutu896@gmail.com>
2023-01-11 12:04:28 +0530

    heim server

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ const repoTenantList = [
 	{ repo: "penknife-app", tenant: ["demo", "practifly", "314ecorp","314einternal"]}, //penknife-app and cli same image. 
 	{ repo: "practifly-server", tenant: ["demo1","demo2","mhill","phs","nephc"] }, //DNS practifly.com //practifly practifly-cli same image.
 	{ repo: "practifly-ui", tenant: ["demo1","demo2","mhill","phs","nephc"] },
+	{ repo: "heim-ui", tenant:["314e"]}, 
+	{ repo: "heim-server",tenant:["314e"]} //heim.314ecorp.com //only server
 ];
 
 const apiUrl = "https://slack.com/api";
